73941405baff9751d31f2bb4ad492030eee086e4
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2018-05-10 20:24:26 +0200

    Python: Look for circllhist.so in more locations

--- src/generatePythonFiles.sh
+++ src/generatePythonFiles.sh
@@ -29,5 +29,18 @@ ffi = FFI()
 ffi.cdef("""
 ${cdef_content}
 """)
-C = ffi.dlopen("libcircllhist.so")
+C = None
+for path in [ # Search for libcircllhist.so
+   "/opt/circonus/lib/libcircllhist.so", # 1. vendor path
+   "./libcircllhist.so", # 2. cwd
+   "libcircllhist.so" # 3. system paths via ld.so
+   ]:
+   try:
+     C = ffi.dlopen(path)
+     break
+   except OSError:
+     pass
+if not C:
+   # let dlopen throw it's error
+   ffi.dlopen("libcirllhist.so")
 EOF
