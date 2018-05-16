c8392314527e9f926ba9e68ebb3693cc0945fb3e
cody271 <cody271@gmx.com>
2018-05-16 11:39:11 -0700

    Explicitly use GNU utilities in FFI shell scripts

--- src/prepareFFI.sh
+++ src/prepareFFI.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 if [ -x /usr/bin/gsed ]; then
     SED=gsed
@@ -6,12 +7,22 @@ else
     SED=sed
 fi
 AWK=awk
+if [ -x /usr/bin/ggrep ]; then
+    GREP=ggrep
+else
+    GREP=grep
+fi
+if [ -x /usr/bin/gegrep ]; then
+    EGREP=gegrep
+else
+    EGREP=egrep
+fi
 
 cat |\
-  grep -v -F '/* FFI_SKIP */' |\
-  grep -v "^$" |\
+  $GREP -v -F '/* FFI_SKIP */' |\
+  $GREP -v "^$" |\
   $SED 's|//.*$||' |\
-  egrep -v "#if|#endif|#define|#include" |\
+  $EGREP -v "#if|#endif|#define|#include" |\
   $SED 's/u_int/uint/g' |\
   $SED 's/API_EXPORT(\([^\)]*\))/\1/g' |\
   $AWK '/typedef struct histogram histogram_t;/ {print "typedef long int ssize_t;"} /./{print}'
