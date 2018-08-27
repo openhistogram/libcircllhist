91a0f207a1dd1a686cf6b8942f8388ec5a1ba541
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2018-08-27 11:54:03 +0200

    Update version information

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup, find_packages
 setup(
     name="circllhist",
-    version="0.1",
+    version="0.2.1",
+    description="Circonus log-linear histogram library",
+    maintainer="Heinrich Hartmann",
+    maintainer_email="heinrich.hartmann@circonus.com",
+    url="https://github.com/circonus-labs/libcircllhist",
     install_requires=['cffi'],
     packages=['circllhist'],
 )
