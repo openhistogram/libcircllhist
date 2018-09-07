c4ed3b0e50f6133b4fbb2e469fc96375883856d3
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2018-09-07 13:57:38 +0200

    Add descriptive error message to python lib

--- src/generatePythonFiles.sh
+++ src/generatePythonFiles.sh
@@ -22,5 +22,11 @@ for path in [ # Search for libcircllhist.so
      pass
 if not C:
    # let dlopen throw it's error
+   print("""
+
+libcircllhist.so was not found on your system.
+Please install libcircllhist from: https://github.com/circonus-labs/libcircllhist/
+
+   """)
    ffi.dlopen("libcircllhist.so")
 EOF
