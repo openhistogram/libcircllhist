317bc796648c8cfc58cba3616e70aab9b7127258
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2018-09-17 18:35:12 +0200

    Fix formatting

--- src/generatePythonFiles.sh
+++ src/generatePythonFiles.sh
@@ -11,22 +11,23 @@ ${STDIN}
 """)
 C = None
 for path in [ # Search for libcircllhist.so
-   "/opt/circonus/lib/libcircllhist.so", # 1. vendor path
-   "./libcircllhist.so", # 2. cwd
-   "libcircllhist.so" # 3. system paths via ld.so
-   ]:
-   try:
-     C = ffi.dlopen(path)
-     break
-   except OSError:
-     pass
+    "/opt/circonus/lib/libcircllhist.so", # 1. vendor path
+    "./libcircllhist.so", # 2. cwd
+    "libcircllhist.so" # 3. system paths via ld.so
+    ]:
+    try:
+        C = ffi.dlopen(path)
+        break
+    except OSError:
+        pass
+
 if not C:
-   # let dlopen throw it's error
-   print("""
+    # let dlopen throw it's error
+    print("""
 
 libcircllhist.so was not found on your system.
 Please install libcircllhist from: https://github.com/circonus-labs/libcircllhist/
 
-   """)
-   ffi.dlopen("libcircllhist.so")
+    """)
+    ffi.dlopen("libcircllhist.so")
 EOF
