9a26f6a59f94cd45d50d0f04abd00fe16b1d9f9e
Heinrich Hartmann <heinrich@heinrichhartmann.com>
2019-05-21 14:12:14 +0200

    Cleanup lua tests

--- src/test/histogram_c_test.lua
+++ src/test/histogram_c_test.lua
@@ -1,72 +1,52 @@
 module(..., package.seeall)
 
+
 local ffi = require('ffi');
-local histo_lib = require "ffi_libcircllhist"
+local circllhist = require "ffi_libcircllhist"
 
-local tester = {}
-local histo
 local writer = io.write
-local success = function() writer("SUCCESS\n") end
-local failed = false
-local fail = function() writer("FAIL\n"); failed = true end
 
-function tester.setup(scratch)
-  histo = histo_lib.hist_alloc();
+local function assert(cond)
+  if not cond then
+    error("Assertion Failed")
+  end
+  writer(".")
 end
 
-function tester.teardown(scratch)
-  histo_lib.hist_free(histo);
+local function setup(scratch)
+  scratch.hist = circllhist.hist_alloc();
 end
 
-function tester.test1_default_empty()
-  writer("test1_default_empty()...")
-  if histo_lib.hist_bucket_count(histo) == 0 then
-    success()
-  else
-    fail()
-  end
+local function teardown(scratch)
+  circllhist.hist_free(scratch.hist);
 end
 
-function tester.test2_single_bucket()
-  writer("test2_single_bucket()...\n")
-  writer("  single insert, single bucket...")
-  histo_lib.hist_insert(histo, 3.1, 5)
-  if histo_lib.hist_bucket_count(histo) == 1 then
-    success()
-  else
-    fail()
-  end
+local tests = {}
+function tests.default_empty(scratch)
+  assert(circllhist.hist_bucket_count(scratch.hist) == 0)
+end
 
-  writer("  single bucket, approx mean...")
-  if histo_lib.hist_approx_mean(histo) == 3.15 then
-    success()
-  else
-    fail()
-  end
+function tests.single_bucket(scratch)
+  local hist = scratch.hist
+  circllhist.hist_insert(hist, 3.1, 5)
+
+  assert(circllhist.hist_bucket_count(hist) == 1)
+  assert(circllhist.hist_approx_mean(hist) == 3.15)
 
   local value =  ffi.new("double[1]")
   local count =  ffi.new("uint64_t[1]")
-  histo_lib.hist_bucket_idx(histo, 0, value, count)
-
-  writer("  single bucket, value read...")
-  if value[0] == 3.1 then
-    success()
-  else
-    fail()
-  end
-
-  writer("  single bucket, count read...")
-  if count[0] == 5 then
-    success()
-  else
-    fail()
-  end
+  circllhist.hist_bucket_idx(hist, 0, value, count)
+  assert(value[0] == 3.1)
+  assert(count[0] == 5)
 end
 
 function runTests()
-    tester.setup()
-    tester.test1_default_empty()
-    tester.test2_single_bucket()
-    tester.teardown()
-    if failed then os.exit(-1) end
+  for test_name,test in pairs(tests) do
+    writer("Test " .. test_name .. ": ")
+    local scratch = {}
+    setup(scratch)
+    test(scratch)
+    teardown(scratch)
+    writer(" SUCCESS\n")
+  end
 end
